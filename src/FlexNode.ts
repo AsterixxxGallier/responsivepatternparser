72ed0cd2147eb3e13c81f5a4164879fbe6c965f7
AsterixxxGallier <asterixxxgallier@aol.com>
2021-10-03 20:52:54 +0200

    Add documentation for FlexNode fields

--- src/FlexNode.ts
+++ src/FlexNode.ts
@@ -1,6 +1,15 @@
 class FlexNode {
+    /**
+     * Name of this node. In the future, this field will be replaced.
+     */
     name: string
+    /**
+     * A list of links that have {@link this} as {@link FlexLink.previous}
+     */
     linksStartingHere: FlexLink[] = []
+    /**
+     * A list of links that have {@link this} as {@link FlexLink.next}
+     */
     linksEndingHere: FlexLink[] = []
 
     constructor(name: string) {
