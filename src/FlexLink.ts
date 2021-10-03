2d48df6860b58c126391d379e13b245eb424ab18
AsterixxxGallier <asterixxxgallier@aol.com>
2021-10-03 15:02:21 +0200

    Add previous, next and distance fields to FlexLink

--- src/FlexLink.ts
+++ src/FlexLink.ts
@@ -1,3 +1,11 @@
 class FlexLink {
+    previous: FlexNode
+    next: FlexNode
+    distance: FlexNode
 
+    constructor(previous: FlexNode, next: FlexNode, distance: FlexNode) {
+        this.previous = previous;
+        this.next = next;
+        this.distance = distance;
+    }
 }
