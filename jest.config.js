b3de156bac2279e68d6399fd33f8f123831c0fa0
AsterixxxGallier <asterixxxgallier@aol.com>
2021-10-04 15:38:15 +0200

    Add test for FlexNode.add

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    moduleFileExtensions: ["ts", "js"],
+    moduleNameMapper: {
+        '^@App/(.*)$': '<rootDir>/src/$1',
+        '^lib/(.*)$': '<rootDir>/common/$1',
+    },
 };
