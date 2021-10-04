100f59bf8c11af52cc34aca0161dfe4f6d40e22a
AsterixxxGallier <asterixxxgallier@aol.com>
2021-10-04 22:15:02 +0200

    Add FlexStructure.parent field

--- src/FlexStructure.ts
+++ src/FlexStructure.ts
@@ -3,9 +3,11 @@ import {FlexNode} from "./FlexNode";
 export class FlexStructure {
 	root: FlexNode
 	end: FlexNode
+	parent: FlexStructure | null
 
-	constructor(root: FlexNode, end: FlexNode) {
+	constructor(root: FlexNode, end: FlexNode, parent: FlexStructure | null = null) {
 		this.root = root;
 		this.end = end;
+		this.parent = parent;
 	}
 }
