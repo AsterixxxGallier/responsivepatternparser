32b17bca332af534f4a1933c33ff7ba525b3d007
AsterixxxGallier <asterixxxgallier@aol.com>
2021-10-07 15:16:12 +0200

    Add FlexNode.traverse method

--- src/FlexNode.ts
+++ src/FlexNode.ts
@@ -37,6 +37,23 @@ export class FlexNode {
 		}
 	}
 
+	traverse(distance: number): [node: FlexNode, distanceToGo: number] {
+		let distanceToGo = distance
+		let current: FlexNode = this
+		while (true) {
+			let longestLink: FlexLink | null = null
+			for (let link of current.linksStartingHere) {
+				if (link.distance <= distanceToGo)
+					if (longestLink == null || longestLink.distance < link.distance)
+						longestLink = link
+			}
+			if (longestLink == null)
+				return [current, distanceToGo]
+			distanceToGo -= longestLink.distance
+			current = longestLink.next
+		}
+	}
+
 	toString() {
 		return `<${this.name} of structure ${this.structure.root.name}.->.${this.structure.root.name}; `
 			+ `starting here: ${this.linksStartingHere}; ending here: ${this.linksEndingHere}>`
